186ad33d384973723eea3f0bd36303f9fd2d2b1f
ysnows <guangjin2.xian@zhaopin.com.cn>
2024-04-11 20:18:53 +0800

    vertex

---
+++ src/chat_vertex.ts
@@ -0,0 +1,18 @@
+
+import { ChatVertexAI } from "@langchain/google-vertexai";
+
+export default function main(options: any) {
+    // change options.temperature to number
+    options.temperature = Number(options.temperature);
+
+    const model = new ChatVertexAI({
+        // modelName: 'claude-3-sonnet@20240229'
+        ...options,
+        authOptions: {
+            projectId: "enconvo"
+        }
+    })
+
+    return model
+}
+
