e6052c278b6fc854e630eb214bbf3339a9f75597
ysnows <guangjin2.xian@zhaopin.com.cn>
2024-04-16 19:27:32 +0800

    optimize tool use

--- src/chat_mistral.ts
+++ src/chat_mistral.ts
@@ -2,14 +2,14 @@ import { ChatMistralAI } from "@langchain/mistralai";
 
 export default function main(options: any) {
     // change options.temperature to number
-    options.temperature = Number(options.temperature);
+    options.temperature = Number(options.temperature.value);
+    options.modelName = options.modelName.value
 
-    // streaming to boolean
-    // options.streaming = options.stream === "true";
+    console.log(options)
 
     const model = new ChatMistralAI({
         ...options,
-        
+
     });
 
     return model
