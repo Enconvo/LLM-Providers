bd4b0cab69ea31ecf56cd4e50d094b772d5d74d3
ysnows <guangjin2.xian@zhaopin.com.cn>
2024-04-16 19:46:22 +0800

    optimize tool use

--- src/chat_openrouter.ts
+++ src/chat_openrouter.ts
@@ -1,10 +1,11 @@
 import { ChatOpenAI } from "langchain/chat_models/openai";
 
 export default function main(options: any) {
+    console.log("options", options);
     // change options.temperature to number
-    options.temperature = Number(options.temperature);
+    options.temperature = Number(options.temperature.value);
     // streaming to boolean
-    options.stream = options.stream === "true";
+    options.modelName = options.modelName.value;
 
 
     let config: any = {
