946e2367ca1bf7f7e5571c1db7333f5ab6c46d87
ysnows <ysnows@163.com>
2024-04-19 08:54:31 +0800

    stop_sequence

--- src/premai.ts
+++ src/premai.ts
@@ -2,7 +2,6 @@ import { ChatPrem } from "@langchain/community/chat_models/premai";
 // import Prem from '@premai/prem-sdk';
 export default function main(options: any) {
     // change options.temperature to number
-    console.log("prem",options)
     options.temperature = Number(options.temperature.value);
 
     const modelOptions = options.model
