336f4670e338f5c37fd4480a38aba40a101a8383
guangjin2.xian <guangjin2.xian@zhaopin.com.cn>
2024-07-30 09:45:50 +0800

    update 4o-mini

--- src/enconvo_models.ts
+++ src/enconvo_models.ts
@@ -16,10 +16,15 @@ export default async function main(req: Request) {
         fs.mkdirSync(modelCacheDir, { recursive: true })
     }
     const modelCachePath = `${modelCacheDir}/${environment.commandName}.json`
-
+    // 如果文件超过一天就重新获取
+    const modelStat = fs.statSync(modelCachePath)
+    const now = new Date()
+    const diff = now.getTime() - modelStat.mtime.getTime()
+    if (diff > 1000 * 60 * 60 * 24) {
+        fs.unlinkSync(modelCachePath)
+    }
 
     fs.existsSync(modelCachePath) || fs.writeFileSync(modelCachePath, '[]')
-
 
     const modelContent = fs.readFileSync(modelCachePath, 'utf8')
     let models = JSON.parse(modelContent)
